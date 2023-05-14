1365bb1b9a85078bccfc6a2138e2214fe716dae5
elrodrics <elrodri1525@gmail.com>
2023-05-14 19:41:12 -0300

    int 3

--- src/main/java/com/portfolio/molinarodrigo/Controller/SkillsController.java
+++ src/main/java/com/portfolio/molinarodrigo/Controller/SkillsController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://portfolio-frontend-rodrigo.web.app")
+@CrossOrigin(origins={"https://portfolio-frontend-rodrigo.web.app/","http://localhost:4200"})
 @RequestMapping("/skill")
 
 public class SkillsController {
